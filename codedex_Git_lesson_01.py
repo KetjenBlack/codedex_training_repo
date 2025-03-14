71ca027f1c3fbcfd0f614f54cf5df08b5259ac54
Catrina Wilson <cat.wilson642@gmail.com>
2025-03-14 15:46:28 -0700

    initial commit

---
+++ codedex_Git_lesson_01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 14 15:04:44 2025
+
+@author: catwi
+file to test codedex.io Git & GitHub lessons
+"""
+
+def silly_goose(message):
+    try:
+        message = message + ', silly goose.'
+        print(message)
+    except:
+        print('Could not decipher secret message.')
+    
